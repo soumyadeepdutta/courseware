4b6b799ca13deb22382ed316872a8fbe593f8c63
soumyadeepdutta <imsoumyadeepdutta@gmail.com>
2021-01-30 20:47:28 +0530

    Database connection fixed

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
-exports.connectDB = async () =>
-  await mongoose
-    .connect(process.env.MONGO_URI, {
+exports.connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
-    })
-    .then(console.log("Connected to MongoDB"))
-    .catch((ex) => {
-      console.log(`DB connection failed: ${ex}`);
-      process.exit(1);
     });
+    console.log("MongoDB Connected ");
+  } catch (ex) {
+    console.log(ex);
+  }
+};

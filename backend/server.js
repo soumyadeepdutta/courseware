b186251ab9d777e4a1c6a756272e5bdf1804289a
soumyadeepdutta <imsoumyadeepdutta@gmail.com>
2021-02-03 19:49:45 +0530

    Access Control Allow Headers

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,15 @@ app.use(express.json());
 
 const courses = require("./routes/course");
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.get("/", (req, res) => {
   res.send("courseware");
 });
